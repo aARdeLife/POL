7c8e99de870e787d46ee2fc88e291dbd64c1365e
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-05-17 15:22:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,108 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const summaryBox = document.createElement('div');
+
+summaryBox.style.position = 'absolute';
+summaryBox.style.padding = '10px';
+summaryBox.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+summaryBox.style.color = 'white';
+summaryBox.style.borderRadius = '5px';
+summaryBox.style.fontSize = '14px';
+summaryBox.style.maxWidth = '250px';
+summaryBox.style.display = 'none';
+
+document.body.appendChild(summaryBox);
+
+async function setupCamera() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 640, height: 480 },
+        audio: false
+    });
+    video.srcObject = stream;
+    video.onloadedmetadata = () => {
+        video.width = video.videoWidth;
+        video.height = video.videoHeight;
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+    };
+
+    return new Promise((resolve) => {
+        video.onloadeddata = () => {
+            resolve(video);
+        };
+    });
+}
+
+function isPointInRect(x, y, rect) {
+    return x >= rect[0] && x <= rect[0] + rect[2] && y >= rect[1] && y <= rect[1] + rect[3];
+}
+
+async function fetchWikipediaSummary(title) {
+    const response = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(title)}`);
+    if (response.ok) {
+        const data = await response.json();
+        return data.extract;
+    } else {
+        return 'No summary available';
+    }
+}
+
+canvas.addEventListener('click', async event => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    for (const prediction of currentPredictions) {
+        if (isPointInRect(x, y, prediction.bbox)) {
+            const summary = await fetchWikipediaSummary(prediction.class);
+            summaryBox.style.display = 'block';
+            summaryBox.style.left = `${prediction.bbox[0] + prediction.bbox[2]}px`;
+            summaryBox.style.top = `${prediction.bbox[1]}px`;
+            summaryBox.textContent = summary;
+            return;
+        }
+    }
+
+    summaryBox.style.display = 'none';
+});
+
+async function detectObjects() {
+    const model = await cocoSsd.load();
+    while (true) {
+        const predictions = await model.detect(video);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.drawImage(video, 0, 0, video.width, video.height);
+        const font = "16px sans-serif";
+        ctx.font = font;
+        ctx.textBaseline = "top";
+        for (const prediction of predictions) {
+            const x = prediction.bbox[0];
+            const y = prediction.bbox[1];
+            const width = prediction.bbox[2];
+            const height = prediction.bbox[3];
+            ctx.strokeStyle = "#00FFFF";
+            ctx.lineWidth = 4;
+            ctx.strokeRect(x, y, width, height);
+            ctx.fillStyle = "#00FFFF";
+            const textWidth = ctx.measureText(prediction.class).width;
+            const textHeight = parseInt(font, 10);
+            ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+                        ctx.fillStyle = "#000000";
+            ctx.fillText(prediction.class, x, y);
+        }
+
+        currentPredictions = predictions;
+        await new Promise(r => setTimeout(r, 100));
+    }
+}
+
+let currentPredictions = [];
+
+(async function() {
+    const videoElement = await setupCamera();
+    videoElement.play();
+    detectObjects();
+})();
+
 
